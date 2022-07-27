472f1ab2d70a538000d49bf813f059af4d06a7bd
valente.dylan92 <valente.dylan92@gmail.com>
2022-07-28 00:15:16 +0200

    Show percentage

--- src/components/CardElement.vue
+++ src/components/CardElement.vue
@@ -2,15 +2,16 @@
     <div class="card-element">
         <h3>{{element.choice1}}</h3>
         <h3>{{element.choice2}}</h3>
-        <p>{{element.result1}}</p>
-        <p>{{element.result2}}</p>
+        <p>{{element.result1}} = {{Math.round(100*element.result1/(element.result1+element.result2))}} %</p>
+        <p>{{element.result2}} = {{Math.round(100*element.result2/(element.result1+element.result2))}} %</p>
+        <!-- <p>{{element.result1 + element.result2}}</p> -->
     </div>
 </template>
 
 <script>
 export default {
   name: 'CardElement',
-  
+
   props:["element", "index"]
 }
 </script>
