760e6a7751af03b334202fe8387fd9f1991c3d3b
valente.dylan92 <valente.dylan92@gmail.com>
2022-07-27 23:46:33 +0200

    Getting Data

---
+++ src/components/CardElement.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card-element">
+        <h3>{{element.choice1}}</h3>
+        <h3>{{element.choice2}}</h3>
+        <p>{{element.result1}}</p>
+        <p>{{element.result2}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CardElement',
+  
+  props:["element", "index"]
+}
+</script>
---
+++ src/views/GameScreen.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <CardElement 
+      v-for="(element,index) in listing"
+      :key="element.id"
+      :index="index"
+      :element="element"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CardElement from '@/components/CardElement.vue'
+
+import list from "../list.json"
+
+export default {
+  name: 'GameScreen',
+  data(){
+    return{
+      listing : list
+    }
+  },
+  components: {
+    CardElement
+  }
+}
+</script>
