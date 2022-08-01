ac6a2f21d89545c2df2fd09de7944eb3a853d077
Dylan Valente <dylan@bigsmile.be>
2022-08-01 15:03:44 +0200

    Test build v2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter } from 'vue-router'
 import GameScreenRather from '@/views/GameScreenRather.vue'
 import GameScreenEver from '@/views/GameScreenEver.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  base: process.env.BASE_URL,
   routes
 })
 
