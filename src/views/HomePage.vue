6377679a187f728a8d974332976c60552a9b6aa4
Dylan Valente <dylan@bigsmile.be>
2022-08-05 22:36:31 +0200

    Quick Homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="home">
+    <h1>Accueil moche fais a l'arrache</h1>
     <nav>
         <router-link to="/tu-preferes">Tu préfères</router-link>  
         <router-link to="/jai-deja">J'ai déjà</router-link>
